73cad7e3d29a1f4146eb604ccd899302f905d1c6
sanchit237 <sanchitnemlekar8@gmail.com>
2022-05-21 14:25:57 +0530

    objects creation and access

---
+++ Basics/myobjects.js
@@ -0,0 +1,25 @@
+let course = {
+    name : 'javascript',
+    author : 'sanchit',
+    price : 2000,
+    description : 'javascript from scratch'
+}
+
+
+console.log(`New course ${course.name} written by ${course.author} priced at Rs. ${course.price} contains ${course.description} `);
+
+console.log(course['name']);
+
+
+let objfunction = function(myobject){
+    // console.log(`Name of the course is ${myobject.name}`);
+    return {
+        case1 : myobject.author,
+        case2 : myobject.price
+    }
+}
+
+let result = objfunction(course);
+
+console.log(result.case1);
+console.log(result.case2);
