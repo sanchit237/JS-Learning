3d2576049f8e268c3edaa78fa806e875d29c136b
sanchit237 <sanchitnemlekar8@gmail.com>
2022-06-13 10:10:23 +0530

    Maps and for of loops

---
+++ Basics/maps.js
@@ -0,0 +1,27 @@
+var john = {
+    name: 'john',
+    age: 20
+}
+var mary = {
+    name: 'mary',
+    age: 30
+}
+
+var sam = {
+		name: 'sam',
+		age: 40
+}
+
+let users = new Map()
+
+users.set('john',john)
+users.set('sam',sam)
+users.set('mary',mary)
+
+// console.log(users)
+
+// console.log(users.get('sam'))
+
+for (const x of users.entries()) {
+	console.log(x)
+}
