8bb9d2cb75ad4cce073c3484be4b5c44e732ba1b
sanchit237 <sanchitnemlekar8@gmail.com>
2022-06-13 22:33:31 +0530

    DOM

--- web-basics/myscript.js
+++ web-basics/myscript.js
@@ -1,1 +1,6 @@
-alert('file loaded')
+console.log(document.body);
+console.log(document.head);
+console.log(document.children);
+console.log(document.width);
+console.log(document.lastElementChild);
+console.log(document.title);
