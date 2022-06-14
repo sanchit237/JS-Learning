3c7fb1681ce280f3360a5783baa3f29bdd573cda
sanchit237 <sanchitnemlekar8@gmail.com>
2022-06-14 22:03:49 +0530

    DOM manipulation

--- web-basics/myscript.js
+++ web-basics/myscript.js
@@ -1,9 +1,16 @@
-console.log(document.body);
-console.log(document.head);
-console.log(document.children);
-console.log(document.width);
-console.log(document.lastElementChild);
-console.log(document.title);
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.children);
+// console.log(document.width);
+// console.log(document.lastElementChild);
+// console.log(document.title);
 
-const targeting = document.querySelectorAll('p')
-console.log(targeting)
+var targeting = document.querySelectorAll('p');
+
+// targeting.forEach(function(p){
+// 	p.innerText = 'hello1';
+// })
+
+//arrow function 
+
+targeting.forEach((p) => p.innerText = 'arrow')
