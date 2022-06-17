a877035284ca89da3c0ff5ff40e596e504f48402
sanchit237 <sanchitnemlekar8@gmail.com>
2022-06-17 14:11:04 +0530

    Advance form validation

--- web-basics/validation.js
+++ web-basics/validation.js
@@ -10,3 +10,12 @@ function validate(e){
 		console.log('Valid input');
 	}
 }
+
+
+document.querySelector('.practice').addEventListener('submit', function(event){
+	event.preventDefault()
+	console.log(event.target.username.value)
+	console.log(event.target.realname.value)
+	event.target.username.value = ''
+	event.target.realname.value = ''
+})
