439c08fc12d45210249f94860beb2218e91ddb11
sanchit237 <sanchitnemlekar8@gmail.com>
2022-06-18 17:31:48 +0530

    Adding functionality done

--- Application/todo.js
+++ Application/todo.js
@@ -11,7 +11,38 @@ remove_btn.addEventListener('click', removeItem)
 
 
 function addItem(){
-	console.log('Add button clicked');
+	var user_input = document.getElementById('input')
+	var user_value = user_input.value
+	var textnode = document.createTextNode(user_value)
+
+	if (user_value === ''){
+		return false
+	}
+	else {
+
+		// create li
+		var li = document.createElement('li')
+		li.setAttribute('class','mycheck')
+
+		//create checkbox
+		var checkbox = document.createElement('input')
+		checkbox.type = 'checkbox'
+		checkbox.setAttribute('id','check')
+		
+		//create Label
+		var label = document.createElement('label')
+
+		//Addition to the list
+	
+		li.appendChild(checkbox)
+		li.appendChild(label)
+		label.appendChild(textnode)
+		ul.insertBefore(li, ul.children[0])
+
+		user_input.value = ''
+	}
+
+	// console.log(user_value)
 }
 
 function removeItem(){
